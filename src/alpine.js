b33dc19463ac202d0f49bf2b40a0c690ba3134b5
Jeff <sagalbot@gmail.com>
2020-10-20 10:55:02 -0700

    feat: alpine compat

---
+++ src/alpine.js
@@ -0,0 +1,66 @@
+import onEntrance from './onEntrance';
+import { animate, onEnterTransitionTo, transitionFrom } from './animate';
+
+/**
+ * Usage: x-init="animateIn('fade')"
+ */
+export const animateIn = (animation, threshold = null) => {
+  return function () {
+    this.$el.classList.add("invisible");
+
+    onEntrance(
+      this.$el,
+      async ($el) => await animate($el, animation),
+      threshold
+    );
+  };
+};
+
+/**
+ * Usage: x-init="transitionTo('scale-x-100')"
+ */
+export const transitionTo = (animation, threshold = null) => {
+  return function () {
+    this.$el.classList.add("invisible");
+
+    onEntrance(
+      this.$el,
+      async ($el) => await onEnterTransitionTo($el, animation),
+      threshold
+    );
+  };
+};
+
+/**
+ * Usage: x-init="enterTransition().from('opacity-0').to('opacity-100')"
+ */
+export const enterTransition = (threshold = null) => ({
+  from: (fromClasses) => ({
+    to: (toClasses) =>
+      function () {
+        this.$el.classList.add("invisible");
+
+        //  Remove any transition classes so that the initial state is not
+        //  transitioned to, rather triggered immediately
+        const transition = [...this.$el.classList].filter(
+          (classname) =>
+            classname.includes("transition") ||
+            classname.includes("duration") ||
+            classname.includes("delay")
+        );
+        this.$el.classList.remove(...transition);
+
+        //  Set initial state
+        this.$el.classList.add(...fromClasses.split(" "));
+
+        setTimeout(async () => {
+          this.$el.classList.add(...transition);
+          onEntrance(
+            this.$el,
+            async ($el) => await transitionFrom($el, fromClasses, toClasses),
+            threshold
+          );
+        }, 0);
+      },
+  }),
+});
