f7c17422bb3c4c6180a3f76cc8b772e94da1a3f1
Jeff <sagalbot@gmail.com>
2021-04-24 12:34:29 -0700

    wip: basic vuejs support as a directive

--- src/breeze.js
+++ src/breeze.js
@@ -1,10 +1,4 @@
-import {
-  shouldAnimate,
-  getToClasses,
-  getFromClasses,
-  getTransitionClasses,
-  scheduleEntrance,
-} from "./utils.js";
+import { parseElementAndScheduleEntrance, shouldAnimate } from "./utils.js";
 
 /**
  * @param $el {HTMLElement}
@@ -16,20 +10,7 @@ export function breeze($el) {
 
   const breeze = [
     ...$el.querySelectorAll(":scope [x-breeze-from], :scope [x-breeze-to]"),
-  ].map((element) => {
-    const transition = getTransitionClasses(element);
-    const from = getFromClasses(element);
-    const to = getToClasses(element);
-
-    element.classList.add("invisible");
-    element.classList.remove(...transition);
-
-    const entrance = [element, from, to, transition];
-
-    scheduleEntrance(...entrance);
-
-    return { element, from, to, transition };
-  });
+  ].map((element) => parseElementAndScheduleEntrance(element));
 
   return { breeze };
 }
